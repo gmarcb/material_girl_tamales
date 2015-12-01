996d7002193b6b09b76188ce209926a7ae4142cf
gmarcb <gmarcb@gmail.com>
2015-11-30 20:28:04 -0600

    first commit

---
+++ app/views/layouts/jsexample.js
@@ -0,0 +1,36 @@
+function myfunction(somemessage, somenumber) {
+	message = somemessage;
+	number = somenumber;
+}
+
+//First Alert of Hello + Goodbye
+
+var message = "Hello World";
+var number = 25;
+alert(message + number);
+
+function myfunction() {
+	message = "Goodbye";
+	number = 10;
+		}
+
+	console.log(conent)
+
+//Change hello and goodbye to different variable
+
+myfunction("This is a new message", 50);
+if (number is > 30)  {
+	alert(message + number);
+}
+
+function repeat(somenumber) {
+	for (var i=0; i < somenumber, i++) {
+		counter = 0
+		myfunction(counter);
+		counter += 1;
+
+	}
+
+}
+
+repeat(7);
